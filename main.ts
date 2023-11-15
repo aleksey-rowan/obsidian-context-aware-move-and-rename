b56cb87cc8a141b54b0fd578a66b6ec431af118c
Aleksey Rowan <79934725+aleksey-rowan@users.noreply.github.com>
2023-11-15 09:24:57 -0500

    chore: cleaned up the code

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ import {
     Editor,
     EditorPosition,
     FileManager,
-    MarkdownView,
     Plugin,
     MarkdownFileInfo,
     TFile,
@@ -11,12 +10,16 @@ import {
 } from "obsidian";
 
 interface ClickableToken {
-    type: string;
+    type: linkTypeEnum;
     text: string;
+    start: EditorPosition;
+    end: EditorPosition;
 }
 
 abstract class EditorExtended extends Editor {
-    abstract getClickableTokenAt(position: EditorPosition): ClickableToken;
+    abstract getClickableTokenAt(
+        position: EditorPosition
+    ): ClickableToken | null;
 }
 
 abstract class AppExtended extends App {
@@ -39,17 +42,46 @@ interface MarkdownFileInfoExtended extends MarkdownFileInfo {
     getFile(): { path: string };
 }
 
+const enum linkTypeEnum {
+    internal = "internal-link",
+    external = "external-link",
+}
+
+type LinkTypes = {
+    [key in linkTypeEnum]: () => void;
+};
+
 export default class ContextAwareRenamePlugin extends Plugin {
     app: AppExtended;
 
     async onload() {
-        // This adds an editor command that can perform some operation on the current editor instance
         this.addCommand({
             id: "rename-file-or-link",
-            name: "File or Link",
-            editorCallback: this.renameFileOrLink.bind(this),
+            name: "Rename file or link",
+            editorCallback: this.renameFileOrLink.bind(this, 123),
+        });
+
+        this.addCommand({
+            id: "rename-link-only",
+            name: "Rename link only",
+            editorCallback: this.renameLinkOnly.bind(this),
+        });
+
+        this.addCommand({
+            id: "move-file-or-link",
+            name: "Move file or link",
+            editorCallback: this.moveFileOrLink.bind(this),
+        });
+
+        this.addCommand({
+            id: "move-link-only",
+            name: "Move link only",
+            editorCallback: this.moveLinkOnly.bind(this),
         });
     }
+
+    renameLinkOnly() {}
+    moveLinkOnly() {}
 
     /**
      * The function `renameFileOrLink` renames a file or triggers a rename dialog for a link in a
@@ -58,44 +90,90 @@ export default class ContextAwareRenamePlugin extends Plugin {
      * class, which represents the editor in which the code is being executed. It provides methods for
      * interacting with the editor, such as getting the current cursor position and getting the
      * clickable token at a specific position.
-     * @param {MarkdownView} view - The `view` parameter is an object of type `MarkdownView`. It
-     * represents the current markdown view in the application and provides methods and properties
-     * related to the view, such as getting the current cursor position and the clickable token at the
-     * cursor position.
      * @returns the result of calling `this.app.fileManager.promptForFileRename(link)`.
      */
-    renameFileOrLink(editor: EditorExtended, view: MarkdownView) {
-        const cursorPosition = editor.getCursor();
-        const token = editor.getClickableTokenAt(cursorPosition);
+    renameFileOrLink(editor: EditorExtended) {
+        const token = this.getClickableToken(editor);
 
+        // rename file since we are not on top of anything clickable
         if (!token) {
-            // rename file since we are not on top of a link
             this.app.commands.executeCommandById("workspace:edit-file-title");
-
             return;
         }
 
-        // ignore external links
-        // TODO: actually trigger "edit link" command
-        if (token.type !== "internal-link") {
+        const linkTypes: LinkTypes = {
+            [linkTypeEnum.external]: () => {
+                // call "edit link" command which just selects the text of the link
+                editor.focus();
+                editor.setSelection(token.start, token.end);
+            },
+            [linkTypeEnum.internal]: () => {
+                const linkTFile = this.getLinkTFile(token);
+
+                // abort if we can't find the link object for some reason
+                if (!linkTFile) {
+                    return;
+                }
+
+                // call to trigger a rename dialog for the link we found
+                this.app.fileManager.promptForFileRename(linkTFile);
+            },
+        };
+
+        linkTypes[token.type]();
+    }
+
+    moveFileOrLink(editor: EditorExtended) {
+        const token = this.getClickableToken(editor);
+
+        // move the open file since we are not on top of anything clickable
+        if (!token) {
+            this.app.commands.executeCommandById("file-explorer:move-file");
             return;
         }
 
+        const linkTypes: LinkTypes = {
+            [linkTypeEnum.external]: () => {},
+            [linkTypeEnum.internal]: () => {
+                // open the folder selector
+                // rename the file
+                this.app.fileManager.renameFile;
+            },
+        };
+
+        linkTypes[token.type]();
+    }
+
+    /**
+     * Retrieves the link object for a given clickable token.
+     * @param {ClickableToken} token - The `token` parameter is of type `ClickableToken`. It represents a
+     * clickable token in the editor, such as a link or a mention.
+     * @returns Returns the `linkTFile` object.
+     */
+    private getLinkTFile(token: ClickableToken) {
         const linkPath = this.normalize(token.text).path;
         const { path: filePath } = this.app.workspace.activeEditor.getFile();
 
         // get the link object itself
-        const link = this.app.metadataCache.getFirstLinkpathDest(
+        const linkTFile = this.app.metadataCache.getFirstLinkpathDest(
             linkPath,
             filePath
         );
+        return linkTFile;
+    }
 
-        if (!link) {
-            return;
-        }
+    /**
+     * Returns the clickable token at the current cursor position in the
+     * editor.
+     * @param {EditorExtended} editor - The `editor` parameter is an instance of the `EditorExtended`
+     * class. It represents the editor component or object that you are working with.
+     * @returns the clickable token at the current cursor position in the editor.
+     */
+    private getClickableToken(editor: EditorExtended) {
+        const cursorPosition = editor.getCursor();
+        const token = editor.getClickableTokenAt(cursorPosition);
 
-        // call to trigger a rename dialog for the link we found
-        return this.app.fileManager.promptForFileRename(link);
+        return token;
     }
 
     /**
@@ -105,7 +183,7 @@ export default class ContextAwareRenamePlugin extends Plugin {
      * @param {string} value - A string value that represents a path or URL.
      * @returns The function `normalize` returns an object with two properties: `path` and `subpath`.
      */
-    normalize(value: string): { path: string; subpath: string } {
+    private normalize(value: string): { path: string; subpath: string } {
         // this replaces a non-breaking space with a regular one
         const nonBreakingSpace = /\u00A0/g;
 
